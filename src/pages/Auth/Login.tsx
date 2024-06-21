7048ade5db3b6016fa15e3b1828f9a7fb612b30e
unker <kfi.unknown@gmail.com>
2024-06-22 00:00:38 +0300

    display error

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -33,9 +33,8 @@ const Login = (): JSX.Element => {
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Log in to your account
           </h2>
-          {error && <div className="text-red-500 text-center mt-2">{error}</div>}
         </div>
-        <form className="mt-8 space-y-6" onSubmit={handleLogin}>
+        <form className="mt-8" onSubmit={handleLogin}>
           <input type="hidden" name="remember" defaultValue="true" />
           <div className="rounded-md shadow-sm -space-y-px">
             <div>
@@ -50,7 +49,13 @@ const Login = (): JSX.Element => {
                 required
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="
+                  appearance-none rounded-none relative block 
+                  w-full px-3 py-2 border border-gray-300 
+                  placeholder-gray-500 text-gray-900 rounded-t-md 
+                  focus:outline-none focus:ring-indigo-500 
+                  focus:border-indigo-500 focus:z-10 sm:text-sm
+                "
                 placeholder="Username"
               />
             </div>
@@ -66,15 +71,34 @@ const Login = (): JSX.Element => {
                 required
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="
+                  appearance-none rounded-none relative block 
+                  w-full px-3 py-2 border border-gray-300 
+                  placeholder-gray-500 text-gray-900 rounded-b-md 
+                  focus:outline-none focus:ring-indigo-500 
+                  focus:border-indigo-500 focus:z-10 sm:text-sm
+                "
                 placeholder="Password"
               />
             </div>
           </div>
+          <div className="h-auto min-h-5 my-1">
+            {error &&
+              <p className="text-red-500 text-sm">
+                {error}
+              </p>
+            }
+          </div>
           <div>
             <button
               type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="
+                group relative w-full flex justify-center 
+                mt-3 py-2 px-4 border border-transparent 
+                text-sm font-medium rounded-md text-white 
+                bg-indigo-600 hover:bg-indigo-700 
+                focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+              "
             >
               Sign in
             </button>
