f81b4c5a5f3a588fb59706d57516f914ff0bb61f
unker <kfi.unknown@gmail.com>
2024-08-04 01:28:35 +0300

    admin panel link

--- src/components/modals/DeleteConfirmationModal.tsx
+++ src/components/modals/DeleteConfirmationModal.tsx
@@ -5,9 +5,11 @@ interface DeleteConfirmationModalProps {
   isOpen: boolean;
   onConfirm: () => void;
   onCancel: () => void;
+  title: string;
+  message: string;
 }
 
-const DeleteConfirmationModal: React.FC<DeleteConfirmationModalProps> = ({ isOpen, onConfirm, onCancel }) => {
+const DeleteConfirmationModal: React.FC<DeleteConfirmationModalProps> = ({ isOpen, onConfirm, onCancel, title, message }) => {
   if (!isOpen) return null;
 
   return (
@@ -25,11 +27,11 @@ const DeleteConfirmationModal: React.FC<DeleteConfirmationModalProps> = ({ isOpe
               </div>
               <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                 <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
-                  Delete File
+                  {title}
                 </h3>
                 <div className="mt-2">
                   <p className="text-sm text-gray-500">
-                    Are you sure you want to delete this file? This action cannot be undone.
+                    {message}
                   </p>
                 </div>
               </div>
--- src/pages/StoragePage.tsx
+++ src/pages/StoragePage.tsx
@@ -1,8 +1,15 @@
 import { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import FileList from "../components/FileList";
 import FileUploadComponent from "../components/FileUploadComponent";
+import { RootState } from '../store/store';
+import { ROUTE_ADMIN } from '../utils/consts';
 
 const StoragePage = (): JSX.Element => {
+  const navigate = useNavigate();
+  const { isAdmin } = useSelector((state: RootState) => state.auth);
+
   const refetchUserFilesRef = useRef<(() => void) | null>(null);
 
   const setRefetchUserFiles = (refetchFunc: () => void) => {
@@ -17,6 +24,18 @@ const StoragePage = (): JSX.Element => {
 
   return (
     <>
+      {isAdmin ? (
+        <div className="w-full flex justify-end px-4">
+          <button
+            onClick={() => navigate(ROUTE_ADMIN)}
+            className="w-48 text-white bg-indigo-600 hover:bg-indigo-700  px-4 py-2 rounded mr-2"
+          >
+            Open Admin panel
+          </button>
+        </div>
+      ) : (
+        <></>
+      )}
       <FileList setRefetchUserFiles={setRefetchUserFiles} />
       <FileUploadComponent onUploadSuccess={handleFileUploadSuccess} />
     </>
